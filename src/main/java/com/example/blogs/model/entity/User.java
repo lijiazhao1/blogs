e5a17fd1e98312e1d250bb5e2436e2d43dfbb9cb
ljz <2924390393@qq.com>
2025-10-21 15:32:11 +0800

    使用lombok，搞一下User实体类

--- src/main/java/com/example/blogs/model/entity/User.java
+++ src/main/java/com/example/blogs/model/entity/User.java
@@ -1,9 +1,19 @@
 package com.example.blogs.model.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
 /**
  * @author ljz
  * @date 2025/10/20
  */
-
-public class User {
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class User implements Serializable {
 }
