2f3451a578eeae6593cf3849c5fa55a555f05ded
ljz <2924390393@qq.com>
2025-10-21 15:57:44 +0800

    完善User实体类

--- src/main/java/com/example/blogs/model/entity/User.java
+++ src/main/java/com/example/blogs/model/entity/User.java
@@ -6,6 +6,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
+import java.time.LocalDateTime;
 
 /**
  * @author ljz
@@ -16,4 +17,15 @@ import java.io.Serializable;
 @AllArgsConstructor
 @NoArgsConstructor
 public class User implements Serializable {
+    private static final long serialVersionUID = 1L;
+    private Long id;
+    private String username;
+    private String password;
+    private String nickname;
+    private String avatar;
+    private String intro;
+    private String email;
+    private Integer status;
+    private LocalDateTime createTime;
+    private LocalDateTime updateTime;
 }
